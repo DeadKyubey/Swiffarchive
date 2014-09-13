b41bec88b3be6b42369452343305281faae03412
shakethat <explosionsbehindme@gmail.com>
2014-09-13 15:45:44 +0300

    Added basic static frontpage

---
+++ app/views/home.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Swiffarchive</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/app.css">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	<link href='http://fonts.googleapis.com/css?family=Roboto:300italic,900,100italic,400italic,400,300,500italic,700italic,100,700,500,900italic' rel='stylesheet' type='text/css'>
+  </head>
+
+  <body>
+
+  	<nav class="navbar navbar-default navbar-fixed-top" role="navigation" id="nav">
+    <div class="container">
+
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapsible">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">swiff archive</a>
+    </div>
+
+    <div class="navbar-collapse collapse" id="navbar-collapsible">
+
+        <ul class="nav navbar-nav navbar-left">
+            <li><a href="#"><span class="glyphicon glyphicon-cloud-upload"></span> Upload</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-list"></span> Categories</a></li>
+        </ul>
+
+        <ul class="nav navbar-nav navbar-right">
+        	<li><a href=""><span class="glyphicon glyphicon-user"></span> Login</a></li>
+        </ul>
+
+        <form class="navbar-form">
+            <div class="form-group" style="display:inline;">
+                <div class="input-group">
+                  <span class="input-group-addon" style="background-color:transparent; border:none"><span class=""></span></span>
+                  <input type="text" class="form-control" placeholder="Search" autofocus>
+                </div>
+            </div>
+        </form>
+    </div>
+
+</div>
+</nav>
+
+<div class="jumbotron">
+  <div class="container">
+        <div class="page-header">
+        <h2>WELCOME TO SWIFF ARCHIVES!</h2>
+      </div>
+      <p class="lead">
+      	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+      	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+      	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    
+  </div>
+</div>
+
+    <div class="footer">
+      <div class="container">
+        <p class="text-muted">swiffarchive (c) 2014</p>
+      </div>
+    </div>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
+
+
+
+
+
+
+
+
+
+
